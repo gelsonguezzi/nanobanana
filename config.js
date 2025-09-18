805eec70673b0d2e9ea15631e8657f5488a0e2e9
gelsonguezzi <geltvs@gmail.com>
2025-09-18 17:17:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Sua chave de API ficará visível para qualquer pessoa que visitar o site.
 // Para este projeto de aprendizado, é uma solução simples.
 // Substitua o texto abaixo pela sua chave de API do Google AI Studio.
-export const API_KEY = 'AIzaSyAHe_a9lL5nwXimoDDQb-xlEZ_6cE2deAM';
+export const API_KEY = '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';
